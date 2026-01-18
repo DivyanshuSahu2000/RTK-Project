10096a5f4316bbf092cb687ffbb7e96a1b3a1ba7
DivyanshuSahu2000 <sdivyanshusahu172@gmail.com>
2026-01-18 23:59:39 +0530

    get media using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { fetchGif, fetchPhotos, fetchVideos } from "./api/mediaApi";
+
+const getPhotos = async () => {
+  const data = await fetchPhotos("cat");
+  console.log(data.results);
+};
+
+const getVideos = async () => {
+  const data = await fetchVideos("cat");
+  console.log(data.videos);
+};
+
+const getGifs = async () => {
+  const data = await fetchGif("cat");
+  console.log(data.results);
+};
+
 function App() {
   return (
     <>
-      <div className="h-screen w-full bg-gray-900 text-white">hh</div>
+      <div className="h-screen w-full bg-gray-900 text-white">
+        hh
+        <button className="bg-red-300 p-3 m-2.5" onClick={getPhotos}>
+          Get Photo
+        </button>
+        <button className="bg-green-300 p-3 m-2.5" onClick={getVideos}>
+          Get Video
+        </button>
+        <button className="bg-blue-300 p-3 m-2.5" onClick={getGifs}>
+          Get Gifs
+        </button>
+      </div>
     </>
   );
 }
